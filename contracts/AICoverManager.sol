d0e4c52abc0cb1b15e2aba7bcded5c934be6dbd5
zeeshanhanif <zeeshanhanif@gmail.com>
2024-08-02 12:09:35 +0500

    Updated AICoverManager to include stakeholder add function and voice NFT add function

--- contracts/AICoverManager.sol
+++ contracts/AICoverManager.sol
@@ -9,8 +9,11 @@ import "hardhat/console.sol";
 contract AICoverManager is Ownable {
     using Strings for uint256;
 
+    uint256 constant public TOTAL_SHARE = 10000; // Equal to 100%
+    string public _url;
+    string public _title;
+    string public _coverId;
     address public manager;
-
     enum StakeholderType{ 
         VOICE,
         VOICE_OWNER,
@@ -25,21 +28,43 @@ contract AICoverManager is Ownable {
         StakeholderType _type;
         uint256 share;
     }
+    // Index will start from 1 so that we can keep the track of length
     uint256 public stakeholderIndex;
-    mapping(uint256 => Stakeholder) public _stakeholder;
+    mapping(uint256 => Stakeholder) public stakeholders;
+    
+    address[] public aiVoiceNFTList;
 
+    event StackholderAdded(uint256 stakeholderIndex, address stakeholderAddress, uint256 _type, uint256 share);
     
     modifier onlyOwnerOrManager() {
         require((owner() == msg.sender) || (manager == msg.sender), "Caller needs to be Owner or Manager");
         _;
     }
 
-    constructor() {
+    constructor(string memory url_, string memory title_, string memory coverId_) {
+        _url = url_;
+        _title = title_;
+        _coverId = coverId_;
         manager = msg.sender;
     }
 
-    function setManager(address _manager) public onlyOwner {
+    function setManager(address _manager) public onlyOwnerOrManager {
         manager = _manager;
+    }
+
+    function addStackholders(Stakeholder[] memory stackeholders_) public {
+        require(stackeholders_.length !=0, "Empty List");
+        uint256 totalShare = 0;
+        for (uint256 i = 0; i < stackeholders_.length; i++) {
+            totalShare+=stackeholders_[i].share;
+            require(totalShare <= TOTAL_SHARE, "Share cannot be more than 100%");
+            stakeholders[++stakeholderIndex] = stackeholders_[i];
+            emit StackholderAdded(stakeholderIndex,stackeholders_[i].userAddress, uint256(stackeholders_[i]._type), stackeholders_[i].share);
+        }
+    }
+
+    function addAIVoiceNFT(address _aiVoiceNFTAddress) public {
+        aiVoiceNFTList.push(_aiVoiceNFTAddress);
     }
 
 }
