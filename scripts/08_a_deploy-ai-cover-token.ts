f39559ec1137b6139d2f40fa595cce2a01b27692
zeeshanhanif <zeeshanhanif@gmail.com>
2024-08-05 13:54:20 +0500

    Added script to deploy AI cover ERC20 contract

---
+++ scripts/08_a_deploy-ai-cover-token.ts
@@ -0,0 +1,24 @@
+import { ethers, network } from "hardhat";
+import { AICoverToken, AICoverToken__factory } from "../typechain-types";
+const addresses = require("./address.json");
+
+async function main() {
+
+  const [owner, addr1] = await ethers.getSigners();
+  console.log("Network = ",network.name);
+
+  const AICoverToken:AICoverToken__factory = await ethers.getContractFactory("AICoverToken");
+  const aiCoverToken:AICoverToken = await AICoverToken.deploy(owner.address, owner.address, owner.address);
+  await aiCoverToken.deployed();
+
+  console.log("AICoverToken deployed to:", aiCoverToken.address);
+
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
