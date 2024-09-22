1c357fb91ba13e70d6d7612a92fba3a5da761c3a
zeeshanhanif <zeeshanhanif@gmail.com>
2024-09-22 23:46:47 +0500

    Updated AI Cover Token

--- contracts/AICoverToken.sol
+++ contracts/AICoverToken.sol
@@ -4,23 +4,36 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AICoverToken is ERC20, AccessControl {
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+contract AICoverToken is ERC20, Ownable {
 
-    constructor(address defaultAdmin, address pauser, address minter)
-        ERC20("AI Cover Token", "DD1")
-    {
-        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _grantRole(PAUSER_ROLE, pauser);
-        _mint(msg.sender, 100000 * 10 ** decimals());
-        _grantRole(MINTER_ROLE, minter);
+    address public minterContractAddress;
+    address public manager;
+
+    modifier onlyOwnerOrManager() {
+        require((owner() == msg.sender) || (manager == msg.sender), "Caller needs to be Owner or Manager");
+        _;
     }
 
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+    modifier onlyOwnerOrManagerOrMinter() {
+        require((owner() == msg.sender) || (manager == msg.sender) || (minterContractAddress == msg.sender), "Caller needs to be Owner or Manager or Minter");
+        _;
+    }
+    constructor() ERC20("AI Cover Token", "DD1") {
+        _mint(msg.sender, 100000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwnerOrManagerOrMinter{
         _mint(to, amount);
+    }
+
+    function setManager(address _manager) public onlyOwnerOrManager {
+        manager = _manager;
+    }
+
+    function setMinter(address _minter) public onlyOwnerOrManager {
+        minterContractAddress = _minter;
     }
 
 }
